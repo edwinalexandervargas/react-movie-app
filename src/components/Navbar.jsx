10472a591d9de6a457c6be9e628cabe26acf4815
Edwin Alexander Vargas <edwinalexandervargas07@gmail.com>
2026-05-22 09:34:45 -0700

    Add Navbar component with background image and dynamic links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav className='relative bg-[url(/movie-theater-signboard.png)] bg-cover bg-[position:50%_60%] bg-no-repeat w-full p-12 flex justify-between items-center overflow-hidden'>
+    <nav className='relative bg-[url(/movie-theater-signboard.png)] bg-cover bg-[position:50%_60%] bg-no-repeat w-full p-10 flex justify-between items-center overflow-hidden'>
     <div className='absolute inset-0 bg-black/50 z-0'></div>
     <img className='h-[70px] w-auto relative z-10' src={logo} alt="" />
     <ul className='flex gap-10 items-center relative z-10'>
