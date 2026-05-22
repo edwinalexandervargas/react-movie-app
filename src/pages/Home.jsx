4fc5cde1fe055d47ed5f7b56caf2a145f4a075be
Edwin Alexander Vargas <edwinalexandervargas07@gmail.com>
2026-05-22 11:24:40 -0700

    Add Home hero section

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='bg-gradient-to-b from-blue-300 to-slate-200 h-screen flex flex-col items-center justify-center'>
+        <h1 className='text-6xl font-bold mb-4'>Welcome to Movies</h1>
+        <h2 className='text-4xl font-semibold mb-4'>Find your favorite films</h2>
+        <button className='bg-blue-700 text-white hover:bg-blue-800 px-6 py-4 border-black border-2 rounded-full hover:bg-blue-800 transition-all duration-300 cursor-pointer'>Browse Films</button>
+    </div>
+  )
+}
+
+export default Home
