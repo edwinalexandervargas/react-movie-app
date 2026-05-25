d5b476445317bbc6166a2f629dc2f47f1b7717d5
Edwin Alexander Vargas <edwinalexandervargas07@gmail.com>
2026-05-24 17:52:06 -0700

    Update nav and footer links, add Browse Films navigation

--- src/constants.js
+++ src/constants.js
@@ -8,13 +8,7 @@ const navLinks = [
     label: "Search",
     path: "/search",
     className: "nav-link text-white text-2xl font-semibold",
-  },
-  {
-    label: "Contact",
-    path: "/contact",
-    className:
-      "bg-white px-6 py-2 text-2xl font-semibold rounded-full hover:opacity-80",
-  },
+  }
 ];
 
 export default navLinks;
@@ -26,18 +20,8 @@ export const footerLinks = [
     className: "hover:opacity-60 transition-all duration-300",
   },
   {
-    label: "About",
-    path: "/about",
-    className: "hover:opacity-60 transition-all duration-300",
-  },
-  {
     label: "Movies",
-    path: "/movies",
+    path: "/search",
     className: "hover:opacity-60 transition-all duration-300",
-  },
-  {
-    label: "Contact",
-    path: "/contact",
-    className: "hover:opacity-60 transition-all duration-300",
-  },
+  }
 ];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+    const navigate = useNavigate()
+
+
   return (
     <div className="bg-gradient-to-b from-blue-300 to-slate-200 h-screen flex flex-col items-center justify-center">
       <h1 className="text-slate-800 text-6xl font-bold mb-4">Welcome to Movies</h1>
       <h2 className="text-slate-800  text-4xl font-semibold mb-4">Find your favorite films</h2>
-      <button className="bg-blue-700 text-white hover:bg-blue-800 px-6 py-4 rounded-full transition-all duration-300 cursor-pointer">
+      <button onClick={() => navigate('/search')} className="bg-blue-700 text-white hover:bg-blue-800 px-6 py-4 rounded-full transition-all duration-300 cursor-pointer">
         Browse Films
       </button>
     </div>
